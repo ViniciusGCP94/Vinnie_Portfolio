14a6144c9e21779df9900b4fdd6868ab7295b258
ViniciusGCP94 <vinniegcp@gmail.com>
2025-09-11 18:08:37 -0300

    feat(TechSkills):adicionando dados das tecnologias para a seção de habilidades.

--- src/data/TechSkills.jsx
+++ src/data/TechSkills.jsx
@@ -1,1 +1,11 @@
-{/*lista de stacks */}
+import { SiReact, SiTailwindcss, SiGit, SiCss3, SiHtml5, SiJavascript, SiGooglegemini } from 'react-icons/si';
+
+export const techIcons = {
+  'React': SiReact,
+  'Tailwind CSS': SiTailwindcss,
+  'Git': SiGit,
+  'CSS3': SiCss3,
+  'HTML5': SiHtml5,
+  'JavaScript': SiJavascript,
+  'Gemini':SiGooglegemini
+};
