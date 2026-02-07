83efbe4da2dcad1c7a113b611d042aa4d593857c
ViniciusGCP94 <vinniegcp@gmail.com>
2026-02-06 21:40:51 -0300

    refactor(ProjectCard.jsx) : Ele vai ficar muito grande, então decidi aplicar SRP dividindo e deixando reutilizável para fácil manutenibilidade futuramente. > - Criando funções utilitárias para centralização da lógica.

---
+++ src/utils/formatters.js
@@ -0,0 +1,12 @@
+export const formatRepoTitle = (repoName) => {
+    return repoName
+        .toLowerCase()
+        .replace(/_/g, ' ')
+        .replace(/\b\w/g, l => l.toUpperCase());
+};
+
+export const getProjectImageUrl = (repoName) => {
+    return `https://raw.githubusercontent.com/ViniciusGCP94/${repoName}/main/capa.png`;
+};
+
+export const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/400x225?text=Sem+Capa';
